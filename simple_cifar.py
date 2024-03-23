ffce204d090e5669b8e41b5227b50d54ead6d893
Thomas D Ahle <thomas@ahle.dk>
2024-03-23 16:59:10 -0700

    updated keller net

--- simple_cifar.py
+++ simple_cifar.py
@@ -241,10 +241,10 @@ net = KellerNet().to(device)
 
 print("Compiling model...")
 # net = torch.compile(net)
-# print("Warmup...")
-# for _ in range(5):
-#     train(net, train_inputs, train_labels, time_limit=1)
-# torch.compile(model_trainbias, mode='max-autotune')
+net = torch.compile(net, mode="max-autotune")
+print("Warmup...")
+for _ in range(5):
+    train(net, train_inputs, train_labels, time_limit=1)
 start_time = time.time()
 print("Start training...")
 n_items = train(net, train_inputs, train_labels, time_limit=5)
