501b3eba77ae193eb7c00cb3915dcfc50c9b1f48
Thomas D Ahle <thomas@ahle.dk>
2024-03-23 18:17:41 -0700

    updated keller net

--- simple_cifar.py
+++ simple_cifar.py
@@ -82,14 +82,6 @@ class Mul(nn.Module):
         return x * self.scale
 
 
-class BatchNorm(nn.BatchNorm2d):
-    def __init__(self, num_features, momentum, eps=1e-12, weight=False, bias=True):
-        super().__init__(num_features, eps=eps, momentum=1 - momentum)
-        self.weight.requires_grad = weight
-        self.bias.requires_grad = bias
-        # Note that PyTorch already initializes the weights to one and bias to zero
-
-
 class Conv(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size=3, padding="same", bias=False):
         super().__init__(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias)
@@ -108,9 +100,9 @@ class ConvGroup(nn.Module):
         super().__init__()
         self.conv1 = Conv(channels_in, channels_out)
         self.pool = nn.MaxPool2d(2)
-        self.norm1 = BatchNorm(channels_out)
+        self.norm1 = nn.BatchNorm2d(channels_out)
         self.conv2 = Conv(channels_out, channels_out)
-        self.norm2 = BatchNorm(channels_out)
+        self.norm2 = nn.BatchNorm2d(channels_out)
         self.activ = nn.GELU()
 
     def forward(self, x):
