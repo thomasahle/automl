ad556165042fcece960b81598c32368dda5da4b8
Thomas D Ahle <thomas@ahle.dk>
2024-03-23 18:22:27 -0700

    updated keller net

--- simple_cifar.py
+++ simple_cifar.py
@@ -150,17 +150,20 @@ def make_net(widths=hyp["net"]["widths"], batchnorm_momentum=hyp["net"]["batchno
 class KellerNet(nn.Module):
     def __init__(self):
         super().__init__()
+        whiten_kernel_size = 2
+        whiten_width = 2 * 3 * whiten_kernel_size**2
         net = nn.Sequential(
-            Conv(3, 64, 3, padding=1),
+            Conv(3, whiten_width, whiten_kernel_size, padding=0, bias=True),
             nn.GELU(),
-            ConvGroup(64, 256, 0.6),
-            ConvGroup(256, 256, 0.6),
-            ConvGroup(256, 256, 0.6),
+            ConvGroup(whiten_width, widths["block1"], batchnorm_momentum),
+            ConvGroup(widths["block1"], widths["block2"], batchnorm_momentum),
+            ConvGroup(widths["block2"], widths["block3"], batchnorm_momentum),
             nn.MaxPool2d(3),
             Flatten(),
-            nn.Linear(256, 10, bias=False),
-            Mul(1 / 9),
+            nn.Linear(widths["block3"], 10, bias=False),
+            Mul(hyp["net"]["scaling_factor"]),
         )
+        net[0].weight.requires_grad = False
         net = net.half().cuda()
         net = net.to(memory_format=torch.channels_last)
         for mod in net.modules():
