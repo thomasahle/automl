428924242fc171f8f6ad0d58a33325ac285bcffb
Thomas D Ahle <thomas@ahle.dk>
2024-03-23 17:24:30 -0700

    updated keller net

--- simple_cifar.py
+++ simple_cifar.py
@@ -32,88 +32,130 @@ class Net(nn.Module):
         return optimizer, scheduler, batch_size
 
 
+## KellerNet
+
+hyp = {
+    "opt": {
+        "train_epochs": 9.9,
+        "batch_size": 1024,
+        "lr": 11.5,  # learning rate per 1024 examples
+        "momentum": 0.85,
+        "weight_decay": 0.0153,  # weight decay per 1024 examples (decoupled from learning rate)
+        "bias_scaler": 64.0,  # scales up learning rate (but not weight decay) for BatchNorm biases
+        "label_smoothing": 0.2,
+        "ema": {
+            "start_epochs": 3,
+            "decay_base": 0.95,
+            "decay_pow": 3.0,
+            "every_n_steps": 5,
+        },
+        "whiten_bias_epochs": 3,  # how many epochs to train the whitening layer bias before freezing
+    },
+    "aug": {
+        "flip": True,
+        "translate": 2,
+    },
+    "net": {
+        "widths": {
+            "block1": 64,
+            "block2": 256,
+            "block3": 256,
+        },
+        "batchnorm_momentum": 0.6,
+        "scaling_factor": 1 / 9,
+        "tta_level": 2,  # the level of test-time augmentation: 0=none, 1=mirror, 2=mirror+translate
+    },
+}
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class Mul(nn.Module):
+    def __init__(self, scale):
+        super().__init__()
+        self.scale = scale
+
+    def forward(self, x):
+        return x * self.scale
+
+
+class BatchNorm(nn.BatchNorm2d):
+    def __init__(self, num_features, momentum, eps=1e-12, weight=False, bias=True):
+        super().__init__(num_features, eps=eps, momentum=1 - momentum)
+        self.weight.requires_grad = weight
+        self.bias.requires_grad = bias
+        # Note that PyTorch already initializes the weights to one and bias to zero
+
+
+class Conv(nn.Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding="same", bias=False):
+        super().__init__(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias)
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        if self.bias is not None:
+            self.bias.data.zero_()
+        # Create an implicit residual via identity initialization
+        w = self.weight.data
+        torch.nn.init.dirac_(w[: w.size(1)])
+
+
+class ConvGroup(nn.Module):
+    def __init__(self, channels_in, channels_out, batchnorm_momentum):
+        super().__init__()
+        self.conv1 = Conv(channels_in, channels_out)
+        self.pool = nn.MaxPool2d(2)
+        self.norm1 = BatchNorm(channels_out, batchnorm_momentum)
+        self.conv2 = Conv(channels_out, channels_out)
+        self.norm2 = BatchNorm(channels_out, batchnorm_momentum)
+        self.activ = nn.GELU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.norm1(x)
+        x = self.activ(x)
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.activ(x)
+        return x
+
+
+def make_net(widths=hyp["net"]["widths"], batchnorm_momentum=hyp["net"]["batchnorm_momentum"]):
+    whiten_kernel_size = 2
+    whiten_width = 2 * 3 * whiten_kernel_size**2
+    net = nn.Sequential(
+        Conv(3, whiten_width, whiten_kernel_size, padding=0, bias=True),
+        nn.GELU(),
+        ConvGroup(whiten_width, widths["block1"], batchnorm_momentum),
+        ConvGroup(widths["block1"], widths["block2"], batchnorm_momentum),
+        ConvGroup(widths["block2"], widths["block3"], batchnorm_momentum),
+        nn.MaxPool2d(3),
+        Flatten(),
+        nn.Linear(widths["block3"], 10, bias=False),
+        Mul(hyp["net"]["scaling_factor"]),
+    )
+    net[0].weight.requires_grad = False
+    net = net.half().cuda()
+    net = net.to(memory_format=torch.channels_last)
+    for mod in net.modules():
+        if isinstance(mod, BatchNorm):
+            mod.float()
+    return net
+
+
 class KellerNet(nn.Module):
-    class Flatten(nn.Module):
-        def forward(self, x):
-            return x.view(x.size(0), -1)
-
-    class Mul(nn.Module):
-        def __init__(self, scale):
-            super().__init__()
-            self.scale = scale
-
-        def forward(self, x):
-            return x * self.scale
-
-    class Conv(nn.Conv2d):
-        def __init__(self, in_channels, out_channels, kernel_size=3, padding="same", bias=False):
-            super().__init__(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias)
-
-        def reset_parameters(self):
-            super().reset_parameters()
-            if self.bias is not None:
-                self.bias.data.zero_()
-            # Create an implicit residual via identity initialization
-            w = self.weight.data
-            torch.nn.init.dirac_(w[: w.size(1)])
-
     def __init__(self):
         super().__init__()
-        act = nn.GELU
-
-        def make_layer(ch_in, ch_out):
-            return nn.Sequential(
-                self.Conv(ch_in, ch_out),
-                nn.MaxPool2d(2),
-                nn.BatchNorm2d(ch_out),
-                act(),
-                self.Conv(ch_out, ch_out),
-                nn.BatchNorm2d(ch_out),
-                act(),
-            )
-
-        net = nn.Sequential(
-            nn.Conv2d(3, 24, kernel_size=2, padding=0, bias=True),
-            act(),
-            make_layer(24, 64),
-            make_layer(64, 256),
-            make_layer(256, 256),
-            nn.MaxPool2d(3),
-            self.Flatten(),
-            nn.Linear(256, 10, bias=False),
-            self.Mul(1 / 9),
-        )
-        net[0].weight.requires_grad = False
-        net = net.half().cuda()
-        net = net.to(memory_format=torch.channels_last)
-        for mod in net.modules():
-            if isinstance(mod, nn.BatchNorm2d):
-                mod.float()
-
-        self.net = net
+        self.net = make_net()
 
     def forward(self, x):
         return self.net(x)
 
     def get_optimizers(self):
-        hyp = {
-            "opt": {
-                "train_epochs": 9.9,
-                "batch_size": 1024,
-                "lr": 11.5,  # learning rate per 1024 examples
-                "momentum": 0.85,
-                "weight_decay": 0.0153,  # weight decay per 1024 examples (decoupled from learning rate)
-                "bias_scaler": 64.0,  # scales up learning rate (but not weight decay) for BatchNorm biases
-                "label_smoothing": 0.2,
-                "ema": {
-                    "start_epochs": 3,
-                    "decay_base": 0.95,
-                    "decay_pow": 3.0,
-                    "every_n_steps": 5,
-                },
-                "whiten_bias_epochs": 3,  # how many epochs to train the whitening layer bias before freezing
-            },
-        }
         batch_size = hyp["opt"]["batch_size"]
 
         def triangle(steps, start=0, end=0, peak=0.5):
