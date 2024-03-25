838bc58d64f4596894581fd43220225d7743273e
Thomas D Ahle <thomas@ahle.dk>
2024-03-25 11:37:39 -0700

    iotest

---
+++ iotest.py
@@ -0,0 +1,29 @@
+import multiprocessing
+import os
+import sys
+import time
+
+def worker(stdout_pipe, child_conn):
+    sys.stdout = os.fdopen(stdout_pipe, "w", buffering=1)
+    print("Started...")
+    for i in range(10):
+        time.sleep(.1)
+        print(i)
+    child_conn.send({"result": 10})
+
+def main():
+    read_stdout, write_stdout = os.pipe()
+    parent_conn, child_conn = multiprocessing.Pipe(duplex=False)
+    p = multiprocessing.Process(target=worker, args=(write_stdout, child_conn))
+    p.start()
+    p.join()
+
+    os.close(write_stdout)
+    with os.fdopen(read_stdout) as stdout:
+        print("Got stdout:", stdout.read())
+
+    print("got object:", parent_conn.recv())
+
+if __name__ == "__main__":
+    main()
+
