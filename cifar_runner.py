e853e72952e14f3657ea3729ef424c27f14c320f
Thomas D Ahle <thomas@ahle.dk>
2024-03-25 10:22:37 -0700

    fixed bugs after refactor

--- cifar_runner.py
+++ cifar_runner.py
@@ -14,16 +14,16 @@ from torch import nn
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(32 * 8 * 8, 64)
         self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x))) # 16x16x16
-        x = self.pool(F.relu(self.conv1(x))) # 32x8x8
-        x = x.reshape(-1, 32 * 8 * 8)
+        x = self.pool(F.relu(self.conv2(x))) # 32x8x8
+        x = x.view(-1, 32 * 8 * 8)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
